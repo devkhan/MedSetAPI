c5ddec226b8c983685f57da4248df082ec9fbb05
devkhan <devesh.khandelwal@outlook.com>
2015-06-25 20:57:29 +0530

    ﻿CustomException + LoginRequestModel added
    CustomException for modifying exception and rethrowing it.
    MoginRequestModel for binding login requests to something.

---
+++ MedSet.RESTAPI/LoginRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedSet.RESTAPI
+{
+	public class LoginRequestModel
+	{
+		// This will be the ID token provided by the client app retrieved from
+		// Google APIs and contains important info, ex. email, client id, etc..
+		public string id_token { get; set; }
+		// Auth provider for searching in our database.
+		public string auth_provider { get; set; }
+		// Auth ID for searching in our database. Email Id in our case.
+		public string auth_id { get; set; }
+	}
+}
