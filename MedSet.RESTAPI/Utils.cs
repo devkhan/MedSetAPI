30526d280ca3daa9432205efbe3409b6c5936032
devkhan <devesh.khandelwal@outlook.com>
2015-07-06 16:30:05 +0530

    md5 hash and unix timestamp utils

--- MedSet.RESTAPI/Utils.cs
+++ MedSet.RESTAPI/Utils.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.UI.WebControls;
 
 namespace MedSet.RESTAPI
 {
+	/// <summary>
+	/// 
+	/// </summary>
 	public class Utils
 	{
 		private static FileStream F = new FileStream(@"E:\Git\MedSetAPI\MedSet.RESTAPI\debug_log.txt", FileMode.OpenOrCreate, FileAccess.Write);
@@ -83,5 +88,41 @@ namespace MedSet.RESTAPI
 				imageFile.Flush();
 			}
 		}
+
+		/// <summary>
+		/// Calculates the m d5 hash.
+		/// </summary>
+		/// <param name="input">The input.</param>
+		/// <returns></returns>
+		public static string CalculateMD5Hash(string input)
+		{
+			// step 1, calculate MD5 hash from input
+			MD5 md5 = MD5.Create();
+			byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+			byte[] hash = md5.ComputeHash(inputBytes);
+
+			// step 2, convert byte array to hex string
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < hash.Length; i++)
+			{
+				sb.Append(hash[i].ToString("X2"));
+			}
+			return sb.ToString();
+		}
+
+		/// <summary>
+		/// Converts DateTime value to UNIX timestamp.
+		/// </summary>
+		/// <param name="value">The DateTime variable.</param>
+		/// <returns></returns>
+		public static long ConvertToTimestamp(DateTime value)
+		{
+			//create Timespan by subtracting the value provided from
+			//the Unix Epoch
+			TimeSpan span = (value - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());
+
+			//return the total seconds (which is a UNIX timestamp)
+			return (long)span.TotalSeconds;
+		} 
 	}
 }
